c47bde629677d6c3276811b04a65739580cebede
werty323 <qwerty323784qwerty@gmail.com>
2024-09-05 17:34:20 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+window['FLS'] = true;
+import "../scss/style.scss";
+import * as flsFunctions from "./files/functions.js";
+flsFunctions.isWebp();
+import * as flsForms from "./files/forms/forms.js";
+import * as flsScroll from "./files/scroll/scroll.js";
+import "./files/script.js";
+////////////////////////////////////////////////
+let say = "hello";
+console.log(say);
+
